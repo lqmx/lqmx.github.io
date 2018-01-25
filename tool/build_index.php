86928f5307720414e43c826ca4968e46d1a65323
lqmx <lmxqmx@foxmail.com>
2018-01-25 22:27:10 +0800

    ..

--- tool/build_index.php
+++ tool/build_index.php
@@ -10,7 +10,7 @@ $base = <<<HTML
     <meta charset="UTF-8">
     <meta name="update_time" content="$v">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>lazy note</title>
     <link rel="stylesheet" href="css/reset.css?v$v">
     <link rel="stylesheet" href="css/style.css?v$v">
