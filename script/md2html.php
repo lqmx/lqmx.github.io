afb73f214b6adaedc5f9f799ddd81682b55a5526
lqmx <lmxqmx@foxmail.com>
2018-02-18 22:58:02 +0800

    ..

--- script/md2html.php
+++ script/md2html.php
@@ -23,6 +23,7 @@ $mds = glob($config['md_dir']."*.md");
 $readme = "../README.md";
 $mds[] = $readme;
 foreach ($mds as $md) {
+
     if($md != $readme) {
         list(, $type, $url, $filename) = explode('.', basename($md, '.md'));
         if($type == 'ing') {
@@ -31,14 +32,26 @@ foreach ($mds as $md) {
     } else {
         $url = $filename = "README";
     }
+
     $markdown = file_get_contents($md);
     $file_md5 = md5($markdown);
+
     if(!$all and isset($file_flag[$url]) and $file_flag[$url] == $file_md5) continue;
+
+    $fp = file($md);
+    $fp = array_slice($fp, 0, count($fp)-1);
+    $nowDate = '###### '.date("Y.m.d", time());
+    $fp[] = $nowDate;
+    $markdown = implode("", $fp);
+
     $file_flag[$url] = $file_md5;
-    echo $filename, PHP_EOL;
     $html = $parsedown->text($markdown);
     $html = sprintf($base, $filename, $html);
+
+    file_put_contents($md, $markdown);
     file_put_contents($config['html_dir'].$url.".html", $html);
+
+    echo $filename, PHP_EOL;
 }
 
 file_put_contents(".fileflag", json_encode($file_flag));
