014297a4c2a8ea5666e9e6edc59555a9b48d68c3
lqmx <lmxqmx@foxmail.com>
2018-02-21 21:38:53 +0800

    ..

--- script/test.php
+++ script/test.php
@@ -1,20 +1,7 @@
 <?php
 
-$readme =  "../README.md";
-$mds[] = $readme;
-foreach ($mds as $md) {
-    if($md != $readme) {
-        list(, $type, $url, $filename) = explode('.', basename($md, '.md'));
-        if($type == 'ing') {
-            continue;
-        }
-    } else {
-        $url = "README";
-    }
-    $markdown = file_get_contents($md);
-    echo $markdown, PHP_EOL;
-//    $html = $parsedown->text($markdown);
-//    $html = sprintf($base, $filename, $html);
-//    file_put_contents($config['html_dir'].$url.".html", $html);
-//    echo $config['html_dir'].$url.".html", PHP_EOL;
+$img = glob("../data/img/hzw.*jpg");
+
+foreach ($img as $v) {
+    echo "![.m23](../img/".basename($v).")", PHP_EOL;
 }
